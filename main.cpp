9a28db7a5bab7e3da3460e02e7be000c93b7bb26
jungle-cat <ustcrevolutionary@gmail.com>
2014-02-28 22:14:48 +0800

    initial project

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2014年2月24日
+ *      Author: Feng
+ */
+
+
+#include <iostream>
+#include <memory>
+
+int main()
+{
+	std::cout << "hello\n" << __cplusplus << std::endl;
+	std::shared_ptr<int> a;
+	return 0;
+}
